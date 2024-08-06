27bcacbfed93c5ef46acbd713c70b696d05535c0
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-08-06 13:48:25 +0200

    Add Header and Layout components

--- assets/pages/VanDetail.jsx
+++ assets/pages/VanDetail.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
+import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
-    const [van, setVan] = React.useState(null);
     const params = useParams()
+    const [van, setVan] = React.useState(null)
+
     React.useEffect(() => {
         fetch(`/api/vans/${params.id}`)
             .then(res => res.json())
-            .then(data => setVan(data.van))
+            .then(data => setVan(data.vans))
     }, [params.id])
 
     return (
@@ -16,7 +16,9 @@ export default function VanDetail() {
             {van ? (
                 <div className="van-detail">
                     <img src={van.imageUrl} />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <i className={`van-type ${van.type} selected`}>
+                        {van.type}
+                    </i>
                     <h2>{van.name}</h2>
                     <p className="van-price"><span>${van.price}</span>/day</p>
                     <p>{van.description}</p>
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "../assets/pages/Home"
 import About from "../assets/pages/About"
 import Vans from "../assets/pages/Vans"
 import VanDetail from "../assets/pages/VanDetail"
+import Layout from "./components/Layout"
 
 import "../server"
  
@@ -12,18 +13,13 @@ import "../server"
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
-      <Routes>
+     <Routes>
+      <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans/" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
+      </Route>
       </Routes>
     </BrowserRouter>
   )
