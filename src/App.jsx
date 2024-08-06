c2952228a36833761677ee88d445a8abff530d8d
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-08-06 16:01:54 +0200

    Add pages: Dashboard, Income, Reviews; component: HostLayout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import React from "react"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "../assets/pages/Home"
 import About from "../assets/pages/About"
-import Vans from "../assets/pages/Vans"
-import VanDetail from "../assets/pages/VanDetail"
+import Vans from "../assets/pages/Vans/Vans"
+import VanDetail from "../assets/pages/Vans/VanDetail"
 import Layout from "./components/Layout"
+import Dashboard from "../assets/pages/Host/Dashboard"
+import Income from "../assets/pages/Host/Income"
+import Reviews from "../assets/pages/Host/Reviews"
+import HostLayout from "./components/HostLayOut"
 
 import "../server"
  
@@ -19,6 +23,12 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/vans/" element={<Vans />} />
         <Route path="/vans/:id" element={<VanDetail />} />
+
+
+        <Route path="/host" element={<HostLayout />} />
+        <Route path="/host" element={<Dashboard />} />
+        <Route path="income" element={<Income />} />
+        <Route path="reviews" element={<Reviews />} />
       </Route>
       </Routes>
     </BrowserRouter>
