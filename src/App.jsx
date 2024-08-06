49ad9625f35fea679e42709368dcf4f8e4205e40
Tafadzwa McNorman <gumbitafadzwa@gmail.com>
2024-08-06 23:06:25 +0200

    Add HostvanDetail and HostVans pages, and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import React from "react"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "../assets/pages/Home"
-import About from "../assets/pages/About"
-import Vans from "../assets/pages/Vans/Vans"
-import VanDetail from "../assets/pages/Vans/VanDetail"
-import Layout from "./components/Layout"
-import Dashboard from "../assets/pages/Host/Dashboard"
-import Income from "../assets/pages/Host/Income"
-import Reviews from "../assets/pages/Host/Reviews"
-import HostLayout from "./components/HostLayOut"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../assets/pages/Home";
+import About from "../assets/pages/About";
+import Vans from "../assets/pages/Vans/Vans";
+import VanDetail from "../assets/pages/Vans/VanDetail";
 
-import "../server"
+import Dashboard from "../assets/pages/Host/Dashboard";
+import Income from "../assets/pages/Host/Income";
+import Reviews from "../assets/pages/Host/Reviews";
+import HostVans from "../assets/pages/Host/HostVans"
+import HostVanDetail from "../assets/pages/Host/HostVanDetail"
+
+import Layout from "../src/components/Layout";
+import HostLayout from "../src/components/HostLayout";
+import "../server";
  
 
 
@@ -18,17 +21,20 @@ function App() {
   return (
     <BrowserRouter>
      <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans/" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="vans" element={<Vans />} />
+        <Route path="vans/:id" element={<VanDetail />} />
 
 
-        <Route path="/host" element={<HostLayout />} />
-        <Route path="/host" element={<Dashboard />} />
+        <Route path="host" element={<HostLayout />}>
+        <Route index element={<Dashboard />} />
         <Route path="income" element={<Income />} />
         <Route path="reviews" element={<Reviews />} />
+        <Route path="vans" element={<HostVans />} />
+        <Route path="vans/:id" element={<HostVanDetail />} />
+      </Route>
       </Route>
       </Routes>
     </BrowserRouter>
